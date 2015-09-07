d45b94f907c875a9cf8289988d5de439f67c43b4
Mani Batra <manibatra2002@gmail.com>
2015-09-07 13:18:51 +1000

    added the model for order history

--- feastbeast/orders/models.py
+++ feastbeast/orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Orders(models.Model):
+	order_id = models.Autofield(primary_key=True)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='user_order')
+	restaurant = models.ForeignKey('restaurants.Restaurant', related_name='restaurant_order')
+	menu_items = models.ManyToManyField('restaurants.MenuItem')
+	total = models.DecimalField(max_digits=10, decimal_places=2)
+	datetime = models.DateTimeField(auto_now_add=True)
+
+
+
