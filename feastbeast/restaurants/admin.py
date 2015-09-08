d72dfbf2d0858370cf467d997ffa9c223012834c
Mani Batra <manibatra2002@gmail.com>
2015-09-08 14:53:14 +1000

    registered all models for admin and did some cosmetic changes to the signup form to line up error spans

--- feastbeast/restaurants/admin.py
+++ feastbeast/restaurants/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Restaurant, DeliveryLocation
+from .models import Restaurant, DeliveryLocation, MenuItem, DeliveryHours, Options
 
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(DeliveryLocation)
+admin.site.register(MenuItem)
+admin.site.register(DeliveryHours)
+admin.site.register(Options)
