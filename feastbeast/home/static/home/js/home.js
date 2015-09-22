8ce97cdcb572a0ad7a892176beb704e57b537f74
Mani Batra <manibatra2002@gmail.com>
2015-09-22 23:56:49 +1000

    added function to make signup button work on pressing enter

---
+++ feastbeast/home/static/home/js/home.js
@@ -0,0 +1,6 @@
+//function to click the signup button on pressing enter
+$("#signupForm input").keyup(function(event) {
+    if (event.keyCode == 13) {
+        $("#signupButton").click();
+    }
+});
