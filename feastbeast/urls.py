9815e13d4d5daadafd4b0b8058160c3fcff6d66c
Mani Batra <manibatra2002@gmail.com>
2015-09-30 01:24:20 +1000

    added the password reset complete form and url config

--- feastbeast/urls.py
+++ feastbeast/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     url(r'^password/reset-password/$', auth_views.password_reset, {'template_name': 'password.html'}),
     url(r'^password/reset-password-done/$', auth_views.password_reset_done, {'template_name': 'password_reset_done.html'}, name="password_reset_done"),
     url(r'^password/reset-password-confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, {'template_name': 'password_reset_confirm.html'}, name="password_reset_confirm"),
-    url(r'^password/reset-password-complete/$', auth_views.password_reset_complete, name="password_reset_complete"),
+    url(r'^password/reset-password-complete/$', auth_views.password_reset_complete, {'template_name': 'password_reset_complete.html'}, name="password_reset_complete"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
