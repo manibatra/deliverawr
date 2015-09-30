d33d1b74b09e70cd94f6cd6990b36e53b2747def
Mani Batra <manibatra2002@gmail.com>
2015-09-30 14:11:51 +1000

    taking the email address from which mails will be sent from the env

--- feastbeast/settings.py
+++ feastbeast/settings.py
@@ -146,7 +146,7 @@ except:
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'manibatra2002@gmail.com'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
 try:
