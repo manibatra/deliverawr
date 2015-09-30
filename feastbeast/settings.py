d9862937442152fa2cc0e102ef375fd1e937236e
Mani Batra <manibatra2002@gmail.com>
2015-09-30 13:42:36 +1000

    hide access id and other stuff when fetching stuff from s3

--- feastbeast/settings.py
+++ feastbeast/settings.py
@@ -129,7 +129,7 @@ try:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
-
+    AWS_QUERYSTRING_AUTH = False
 
     STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
     MEDIA_URL = STATIC_URL + 'media/'
