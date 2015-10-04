a326974b7b8bced9bb820c577734a320e52323f4
Mani Batra <manibatra2002@gmail.com>
2015-10-04 18:04:10 +1100

    changed the subject of the mail

--- restaurants/utils.py
+++ restaurants/utils.py
@@ -7,6 +7,6 @@ def send_mail_deliverawr(text_to_send):
         auth=("api", "key-37d788bd314bf02a7fbb52dfe24efe4a"),
         data={"from": "Deliverawr <mailgun@sandboxc0c1bcb688814d6c94674b7d42ca1018.mailgun.org>",
               "to": ["manibatra2002@gmail.com"],
-              "subject": "Thank you for ordering",
+              "subject": "Expression of Interest - Business",
               "text": text_to_send
 	})
