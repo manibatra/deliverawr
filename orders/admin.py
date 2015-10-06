be76f98983b6cf0200e28af2d180caa3c39f8552
Mani Batra <manibatra2002@gmail.com>
2015-10-06 15:58:51 +1100

    made the order history fields ready only for now

--- orders/admin.py
+++ orders/admin.py
@@ -2,7 +2,22 @@ from django.contrib import admin
 from .models import UserOrder, Detail
 
 # Register your models here.
+class DetailInline(admin.TabularInline):
+	model = Detail
+	extra = 0
+
+	def get_readonly_fields(self, request, obj=None):
+		if obj: # editing an existing object
+			return self.readonly_fields + ('menu_item', 'add_ons', 'removed', 'subtotal')
+		return self.readonly_fields
+
 class UserOrderAdmin(admin.ModelAdmin):
 	list_filter = ('restaurant',)
+	inlines = [DetailInline,]
+
+	def get_readonly_fields(self, request, obj=None):
+		if obj: # editing an existing object
+			return self.readonly_fields + ('user', 'restaurant', 'order_time', 'total_price', 'delivery_address', 'phone_no')
+		return self.readonly_fields
 
 admin.site.register(UserOrder, UserOrderAdmin)
