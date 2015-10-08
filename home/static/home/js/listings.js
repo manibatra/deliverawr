b45112d7fa4d6f99f1ddcaab4e20f1118c0aa2c0
Mani Batra <manibatra2002@gmail.com>
2015-10-08 21:46:49 +1100

    showing all restaurants, even closed oneswith an alert indicating the same on clicking

--- home/static/home/js/listings.js
+++ home/static/home/js/listings.js
@@ -1,12 +1,3 @@
-$(document).ready(function() {
-    if ($(".restaurant-listings .demo-card-square").length) {
-
-    } else {
-        $(".restaurant-listings").append('\
-					<div class="row text-center pretty-text center-block">\
-						<span class="sorry-text">\
-							Sorry. All restaurants are closed for delivery at the moment. Pre-ordering coming soon!!\
-						</span>\
-					</div>');
-    }
+$("div.demo-card-square").on('click', function() {
+    alert("The restaurant is closed for delivery at the moment. Pre-ordering coming soon!!")
 })
