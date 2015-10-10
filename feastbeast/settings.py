f7e0c7f11e58e8eefcb4b97231849166656504df
Mani Batra <manibatra2002@gmail.com>
2015-10-10 16:31:24 +1100

    added ssl security for production

--- feastbeast/settings.py
+++ feastbeast/settings.py
@@ -41,6 +41,9 @@ ALLOWED_HOSTS = ['*']
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 # Application definition
